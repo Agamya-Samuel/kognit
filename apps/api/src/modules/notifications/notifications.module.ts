fdbb717bc5029db66f128883c740a31b6a867eae
Agamya Samuel <github@agamya.dev>
2026-05-13 19:59:23 +0530

    feat: add NotificationsRepository provider to notifications module

--- apps/api/src/modules/notifications/notifications.module.ts
+++ apps/api/src/modules/notifications/notifications.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { DRIZZLE_DB } from '../../db/database.module';
+import { NotificationsRepository } from '../../db/repositories/notifications.repository';
 
-@Module({})
+const repositories = [
+  {
+    provide: NotificationsRepository,
+    useFactory: (db: any) => new NotificationsRepository(db),
+    inject: [DRIZZLE_DB],
+  },
+];
+
+@Module({
+  providers: [...repositories],
+  exports: [NotificationsRepository],
+})
 export class NotificationsModule {}
